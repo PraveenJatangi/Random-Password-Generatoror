f580ff1ba0fd2d687f5e85313df6149538c5eddc
Praveen <praveenrams766@gmail.com>
2023-09-14 13:03:17 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let output = document.getElementById("output")
+let passwordCount = document.getElementById("passwordCount")
+let lowercaseSel = document.getElementById("lowercase")
+let numberSel = document.getElementById("number")
+let symbolSel = document.getElementById("symbol")
+let button = document.getElementById("passowordGen")
+let clipboard = document.getElementsByClassName("flipboard")[0]
+button.addEventListener("click",passwordGenerator)
+clipboard.addEventListener("click",copyText)
+let lowerCase = "abcdefghijklmnopqrstuvwxyz"
+let number =  "1234567890"
+let symbol = "#@$%!_-?{}[]&^"
+let finalOutput = ""
+let count = 0
+
+
+function passwordGenerator (){
+let result = ""
+let randomPass =""
+lowercaseSel.checked && (result += lowerCase)
+numberSel.checked && (result +=  number)
+symbolSel.checked && (result += symbol)
+
+ for (let i =0; i<passwordCount.value; i++){
+randomPass += result[Math.floor(Math.random()*result.length)]
+ }
+output.textContent = randomPass
+}
+
+function copyText (){
+    navigator.clipboard.writeText(output.textContent).then(() => {
+        alert("Copied to clipboard");
+    });
+}
